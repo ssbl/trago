5c708482d3991e76d330c987a3ccbffbe494126d
Shubham Lagwankar <shubhu105@gmail.com>
2017-01-15 23:36:03 +0530

    db functions: generate random replica id on the first run

--- db.go
+++ db.go
@@ -1,14 +1,59 @@
 package main
 
 import (
+	"math/rand"
 	"fmt"
 	"io/ioutil"
 	"os"
-	// "path/filepath"
+	"time"
 )
 
+const (
+	TRADB = ".trago.db"
+	bytes = "abcdefghijklmnopqrstuvwxyz1234567890"
+)
+
+type TraDb struct {
+	replicaId string
+	version int
+	files map[string]FileState
+}
+
+type FileState struct {
+	size int
+	mtime time.Time
+	// TODO: use a hash as well
+}
+
 func main() {
-	findFiles("./")
+	db, _ := parseDbFile()
+
+	fmt.Println(db)
+}
+
+func parseDbFile() (TraDb, error) {
+	tradb := TraDb{}
+
+	_, err := ioutil.ReadFile(TRADB)
+	if os.IsNotExist(err) {
+		fmt.Println("didn't find .trago.db")
+		tradb = createDb()
+	} else {
+		fmt.Println(err)
+	}
+
+	return tradb, nil
+}
+
+func createDb() TraDb {
+	replicaId := make([]byte, 16)
+	version := 
+
+	for i, _ := range replicaId {
+		replicaId[i] = bytes[rand.Intn(len(bytes))]
+	}
+
+	return TraDb{}
 }
 
 func findFiles(path string) {
