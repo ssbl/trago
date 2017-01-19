7ffb08ec96f11f0a54f9d69ed20c46e8812a8f9c
Shubham Lagwankar <shubhu105@gmail.com>
2017-01-19 08:36:47 +0530

    cleanup

--- db.go
+++ db.go
@@ -157,15 +157,6 @@ func writeDb(tradb TraDb) {
 	checkError(err)
 }
 
-func findFiles(path string) {
-	files, err := ioutil.ReadDir(path)
-	checkError(err)
-
-	for _, file := range files {
-		fmt.Println(file.Name())
-	}
-}
-
 func checkError(err error) {
 	if err != nil {
 		fmt.Println(err)
