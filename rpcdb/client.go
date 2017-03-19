b71fc7ffd5460f535ef0cebe991228c70c9b7b6f
Shubham Lagwankar <shubhu105@gmail.com>
2017-03-18 21:37:10 -0400

    more rpc tinkering

--- rpcdb/client.go
+++ rpcdb/client.go
@@ -2,24 +2,68 @@
 package main
 
 import (
-	"net/rpc"
+	"fmt"
 	"log"
+	"net/rpc"
 
 	"github.com/ssbl/trago/db"
 )
 
 
-func main() {
+const (
+	LOCALDIR  = "../a"
+	REMOTEDIR = "../b"
+)	
 
-	client, err := rpc.DialHTTP("tcp", "localhost:8999")
+func main() {
+	localClient, err := rpc.DialHTTP("tcp", "localhost:8999")
+	if err != nil {
+		log.Fatal(err)
+	}
+	remoteClient, err := rpc.DialHTTP("tcp", "localhost:8998")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = startSrv(localClient, LOCALDIR)
+	if err != nil {
+		fmt.Println("failed here")
+		log.Fatal(err)
+	}
+	err = startSrv(remoteClient, REMOTEDIR)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	args := 1
-	reply := db.TraDb{}
-	err = client.Call("TraSrv.GetDb", &args, reply)
+	localDb := db.TraDb{}
+	err = localClient.Call("TraSrv.GetDb", &args, &localDb)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Reply from local trasrv:\n%v\n", localDb)
+
+	remoteDb := db.TraDb{}
+	err = remoteClient.Call("TraSrv.GetDb", &args, &remoteDb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Reply from remote trasrv:\n%v\n", remoteDb)
+
+	fmt.Println("Comparing local with remote...")
+	fmt.Println(localDb.Compare(&remoteDb))
+	fmt.Println("Comparing remote with local...")
+	fmt.Println(remoteDb.Compare(&localDb))
+}
+
+func startSrv(client *rpc.Client, dir string) error {
+	var reply int
+
+	err := client.Call("TraSrv.InitSrv", &dir, &reply)
+	if err == db.FileNotFound {
+		return nil
+	}
+	return err
 }
--- rpcdb/server.go
+++ rpcdb/server.go
@@ -2,19 +2,56 @@
 package main
 
 import (
-	"net/rpc"
-	"net/http"
 	"errors"
 	"log"
+	"net/rpc"
+	"net/http"
+	"os"
 
 	"github.com/ssbl/trago/db"
 )
 
-
 type TraSrv int
 
+var (
+	localDb     *db.TraDb
+	initialized bool
+)
+
+const (
+	PORT = ":8999"
+)
+
+
 func (t *TraSrv) GetDb(args *int, reply *db.TraDb) error {
-	return errors.New("error fetching db")
+	if !initialized {
+		return errors.New("uninitialized")
+	} else {
+		*reply = *localDb
+		return nil
+	}
+}
+
+func (t *TraSrv) PutDb(args *db.TraDb, reply *int) error {
+	localDb = &db.TraDb{}
+	*localDb = *args
+	return nil
+}
+
+func (t *TraSrv) InitSrv(dir *string, reply *int) error {
+	err := os.Chdir(*dir)
+	assert(err, "Error changing to directory: %s\n", err)
+
+	localDb, err = db.ParseFile()
+	if err == db.FileNotFound {
+		initialized = true
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	initialized = true
+	return nil
 }
 
 func main() {
@@ -22,5 +59,13 @@ func main() {
 	rpc.Register(trasrv)
 	rpc.HandleHTTP()
 
-	log.Fatal(http.ListenAndServe(":8999", nil))
+	port := ":" + os.Args[1]
+
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func assert(err error, format string, args ...interface{}) {
+	if err != nil {
+		log.Fatalf(format, args)
+	}
 }
